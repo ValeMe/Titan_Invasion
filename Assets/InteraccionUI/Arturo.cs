19c42409a8f5d18646c7e5c81dea550494b2f035
ArturoHoyos21 <arturojose21@hotmail.com>
2020-04-20 21:36:39 -0500

    Segundo Commit

---
+++ Assets/InteraccionUI/Arturo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arturo : MonoBehaviour
+{
+    int uno = 0;
+    double dos = 0f;
+    string hola = ""; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
